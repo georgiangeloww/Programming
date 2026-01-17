92721f6a697305f9ed1617a18df01b003f13b377
georgiangeloww <georgigeorgievangelov5@gmail.com>
2026-01-17 19:30:49 +0200

    Program printing triangle chars from 1 to n

--- reccursion/code/triangle.cpp
+++ reccursion/code/triangle.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 
+
+void printChar(char ch, int count);
+void triangle(int n, char ch, int row);
+
 int main(){
 
     int n;
-    char ch;
+    char ch;    
     std::cin >> n;
     std::cin >> ch;
 
+    triangle(n, ch, 1);
+
     
     return 0;
+}
+
+
+void printChar(char ch, int count){
+    if(count == 0) return;
+
+    std::cout << ch << " ";
+    return printChar(ch, count - 1);
+}
+
+void triangle(int n, char ch, int row){
+    if(row > n){
+        return;
+    }
+
+        printChar(ch, row);
+        std::cout << "\n";
+        
+        return triangle(n, ch, row + 1);
 }
