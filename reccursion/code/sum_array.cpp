5855f2cf03e89607a020d407fa809b7e6b309ef1
georgiangeloww <georgigeorgievangelov5@gmail.com>
2026-01-19 07:50:41 +0200

    Add function for sum of dynamic array with reccursion

--- reccursion/code/sum_array.cpp
+++ reccursion/code/sum_array.cpp
@@ -5,20 +5,27 @@ void printArr(int * arr, int n, int i);
 int sumArr(int * arr, int n, int i);
 
 int main(){
-    int arr[5];
+    int n;
+    std::cin >> n;
+    
+    int* arr = new int[n];
 
-    inputArr(arr, 5, 0);
-    printArr(arr, 5, 0);
+    inputArr(arr, n, 0);
+    printArr(arr, n, 0);
     std::cout << std::endl;
+    std::cout << sumArr(arr, n, 0);
+    
 
+    if(arr != nullptr){
+        delete[]arr;
+    }
+    arr = nullptr;
 
     return 0;
 }
 
 void inputArr(int * arr, int n, int i){
-    if(i == n){
-        return;
-    }
+    if(i == n) return;
 
     std::cin >> arr[i];
     return inputArr(arr, n, i + 1);
@@ -29,4 +36,10 @@ void printArr(int * arr, int n, int i){
 
     std::cout << arr[i] << " ";
     return printArr(arr, n, i + 1);
+}
+
+int sumArr(int* arr, int n, int i){
+    if(i == n) return 0;
+
+    return arr[i] + sumArr(arr, n, i + 1);
 }
