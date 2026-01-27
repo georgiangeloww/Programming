a6dde9790401a3ade010249268c8d2ba3b2f45e6
georgiangeloww <georgigeorgievangelov5@gmail.com>
2026-01-27 20:04:49 +0200

    Add the type Message

---
+++ struct/code/spy_messages.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#define endl '\n'
+using std::cin;
+using std::cout;
+
+
+struct Message{
+    std::string name;
+    std::string text;
+    int levelSec;
+    void input();
+    void output();
+
+};
+
+int main(){
+
+    return 0;
+}
+
