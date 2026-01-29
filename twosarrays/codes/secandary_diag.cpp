c7c38f8a8e1101fc3d7f1b68903fa76933b480f0
georgiangeloww <georgigeorgievangelov5@gmail.com>
2026-01-29 22:51:14 +0200

    Store the sum of the elements on the secondary diagonal

--- twosarrays/codes/secandary_diag.cpp
+++ twosarrays/codes/secandary_diag.cpp
@@ -19,6 +19,15 @@ int main(){
         }
     }
 
+
+    int sum_sec_d = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = n - i - 1; j < n - i; j++){
+            sum_sec_d += arr[i][j];
+        }
+    }
+
+
     for(int j = 0; j < n; j++){
         if(arr[j] != nullptr){
             delete[] arr[j];
