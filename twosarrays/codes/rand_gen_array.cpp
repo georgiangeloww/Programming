59364011aec29a403bbacbea6abceaf97ca4ae3d
georgiangeloww <georgigeorgievangelov5@gmail.com>
2026-01-30 12:15:18 +0200

    Input randomly chosen number for an 2d array

--- twosarrays/codes/rand_gen_array.cpp
+++ twosarrays/codes/rand_gen_array.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
 
 int main(){
     int n, m;
@@ -9,6 +13,16 @@ int main(){
         rand_arr[j] = new int [m];
     }
 
+    srand((unsigned int)time(0));
+
+    int max = 112;
+    int min = -12;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            rand_arr[i][j] = rand() % (max - min + 1) + min;
+        }
+    }
 
 
     if(rand_arr != nullptr){
@@ -19,4 +33,6 @@ int main(){
         delete[] rand_arr;
         rand_arr = nullptr;
     }
+
+    return 0;
 }
