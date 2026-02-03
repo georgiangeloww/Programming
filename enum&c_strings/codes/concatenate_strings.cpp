25d255899e7e8d925533db1b8baf80dacb2e828e
georgiangeloww <georgigeorgievangelov5@gmail.com>
2026-02-03 22:40:25 +0200

    Add function for concatenate strings

--- enum&c_strings/codes/concatenate_strings.cpp
+++ enum&c_strings/codes/concatenate_strings.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 int const MAX = 10;
 
+char* concatenateStrings(char** words, int n);
 
 int main(){
     int n;
@@ -14,6 +15,10 @@ int main(){
         std::cin >> strings[i];
     }
 
+    char* finalResult = concatenateStrings(strings, n);
+
+    std::cout << "Final result: " << finalResult << std::endl;
+
 
     if(strings != nullptr){
         for(int i = 0; i < n; i++){
@@ -23,8 +28,27 @@ int main(){
         delete[] strings;
         strings = nullptr;
 
+        delete[] finalResult; 
+        finalResult = nullptr;  
+
     }
 
     return 0;
 }
 
+char* concatenateStrings(char** words, int n){
+    size_t totalLength = 0;
+    
+    for(int i = 0; i < n; i++){
+        totalLength += std::strlen(words[i]);
+    }
+
+    char* result = new char[totalLength + 1];
+    result[0] = '\0';
+
+    for(int i = 0; i < n; i++){
+        std::strcat(result, words[i]);
+    }
+
+    return result;
+}
