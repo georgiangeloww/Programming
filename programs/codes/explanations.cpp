111681e450bbf5fff2118d8f4eb0a464434f2f1c
georgiangeloww <georgigeorgievangelov5@gmail.com>
2026-02-05 23:25:34 +0200

    docs: add explanations for tokens, literals and commands

---
+++ programs/codes/explanations.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Tokens mean: From the alphabet of the programming language were formed the words for the programming language."
+    "They are the smallest meaningful units in a program." << std::endl;
+
+    std::cout << "Literals mean: data used for representing fixed point values that we can directly use in the code " << std::endl;
+
+    std::cout << "Commands mean: behavioral design pattern that converts requests or simple operations into objects";
+
+    return 0;
+}
