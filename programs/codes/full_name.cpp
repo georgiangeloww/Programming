48ca9b34984066b947867d2be0f2198073745b4f
georgiangeloww <georgigeorgievangelov5@gmail.com>
2026-02-05 23:27:44 +0200

    feat: add printing three names input

---
+++ programs/codes/full_name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+
+int main(){
+    std::string first_name, second_name, third_name;
+    std::cin >> first_name;
+    std::cin >> second_name;
+    std::cin >> third_name;
+
+
+    std::cout << "Your three names are: " << first_name << " " << second_name << " " << third_name;
+
+    return 0;
+}
